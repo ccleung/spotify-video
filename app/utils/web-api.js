63f061a6313ec9561bc2d413afc737f2851eeb2b
ccleung <clemsquared@gmail.com>
2016-02-22 21:16:58 -0500

    web api

---
+++ app/utils/web-api.js
@@ -0,0 +1,28 @@
+import $ from 'jquery';
+
+const apiKey = "AIzaSyB3iMwuO5nwMtwVU9pzrcIhWeY_c0XLPpg";
+
+export const getYouTubeVideoID = (query) => {
+	var query = escape(query);
+	var params = {
+		key: apiKey,
+		type: 'video',
+		part: 'id',
+		maxResults: 1,
+		q: query,
+
+	};
+
+	return $.ajax({
+	  url: "https://www.googleapis.com/youtube/v3/search",
+	  data: params,
+	  dataType: 'json',
+	  success: (data) => {
+	  	debugger;
+	    return data;
+	  },
+	  error: (xhr, status, err) => {
+	    console.error(status, err.toString());
+	  }
+	});
+}
