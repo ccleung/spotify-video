be3eb811dd888a55c02e1268622478d58d66c2c4
ccleung <clemsquared@gmail.com>
2016-05-29 22:00:27 -0400

    use classnames module

--- app/components/track.js
+++ app/components/track.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import styles from '../styles/track.css'
+import styles from '../styles/track.css';
+import classNames from 'classnames/bind';
 
 export default class Track extends React.Component {
   render() {
-    var trackClasses = styles.track;
-    if (this.props.isSelected) {
-      trackClasses += ' ' + styles.selected;
-    }
+    var cx = classNames.bind(styles);
+
+    var trackClasses = cx({
+       track: true,
+       selected: this.props.isSelected
+    })
     return (
       <div className={trackClasses} onClick={this.props.onClick}>
         {this.props.artist} - {this.props.name}
